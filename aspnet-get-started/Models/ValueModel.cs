c45a9b34d76881720974fbf6b7daf157d5e18883
arunv707 <arunv707@gmail.com>
2025-10-17 14:24:05 +0530

    Update ValueModel.cs

--- aspnet-get-started/Models/ValueModel.cs
+++ aspnet-get-started/Models/ValueModel.cs
@@ -1,7 +1,31 @@
-namespace G2App.Models
-{
-    public class ValueModel
+    public DatabaseValueController()
     {
-        public string DisplayValue { get; set; }
+        connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SqlConnectionString"].ConnectionString;
+    }
+
+    public ActionResult Index()
+    {
+        string value = "No data found";
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT TOP 2 columnx FROM tablex";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    object result = command.ExecuteScalar();
+                    value = result != null ? result.ToString() : "No data found";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine($"Database Error: {ex.Message}");
+            value = $"Error: {ex.Message}";
+        }
+
+        var model = new DatabaseValue { Value = value };
+        return View(model);
     }
 }
